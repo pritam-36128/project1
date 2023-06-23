a46d565811ac9b92132453623c7304349096bd6b
pritam <pritam.pk36128@gmail.com>
2023-06-23 15:57:20 +0530

    commit

--- EventORG/settings.py
+++ EventORG/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8#&dwli2*6tou!*p8)4*b^w#2aebw@oj$rr=!yf@*mqwh0(jh&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh']
 
 
 # Application definition
@@ -72,17 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'EventORG.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -123,3 +112,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
